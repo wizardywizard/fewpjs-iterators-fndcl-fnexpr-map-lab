260795ccd6ce922ca984bc0934c770ea2a4811f7
wizardywizard <littledavid175@gmail.com>
2020-02-16 14:39:26 -0800

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => (tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))) 
 }
+
